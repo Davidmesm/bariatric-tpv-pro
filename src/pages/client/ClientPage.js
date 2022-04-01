2812ec2ec5f26e9d609149a0500e528457cbc06a
dmoreno <david.mesm@gmail.com>
2022-04-01 09:15:51 -0600

    Update clients size grid

--- src/pages/client/ClientPage.js
+++ src/pages/client/ClientPage.js
@@ -363,7 +363,7 @@ const ClientPage = () => {
             data={exportData}
             headers={exportHeaders}
             addFunction={addFunction}>
-            <Box height="400px">
+            <Box height="570px">
                 <Box display="flex" height="100%">
                     <Box flexGrow={1}>
                         <DataGrid
